0fea1c5358735ae19d991391ca9c0b765b92b1b0
Your Name <desireirankundawilliams@gmail.com>
2021-08-17 00:37:37 +0200

    added a product component

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import Loading from "./Loading";
-// import Loading from "./Loading";
+
 
 const ProductDetail = () => {
   const [detail, setDetail] = useState({});
@@ -11,7 +10,7 @@ const ProductDetail = () => {
   async function fetchProductDetails() {
     setLoading(true);
     axios
-      .get("https://fakestoreapi.com/products/1")
+      .get("https://fakestoreapi.com/products/2")
       .then((res) => {
         console.log(res);
         setDetail(res.data);
@@ -36,28 +35,57 @@ const ProductDetail = () => {
   // }
 
   return (
-    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+    // <div className="max-w-md mx-auto bg-white rounded-xl shadow-lg overflow-hidden md:max-w-6xl lg:h-96">
+    //   {loading ? (
+    //     "loading"
+    //   ) : (
+    //     <div className="md:flex">
+    //       <div className="md:flex-shrink-0">
+    //         <img
+    //           className="h-60 w-full object-cover md:h-full lg:h-full  md:w-48"
+    //           src={detail.image}
+    //           alt={detail.title}
+    //         />
+    //       </div>
+    //       <div className="p-8 text-center">
+    //         <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+    //           <h1> {detail.title}</h1>
+    //         </div>
+    //         <p> price: K {detail.price} </p>
+    //         <p> Category: {detail.category} </p>
+    //         <p className="mt-2 text-gray-500">
+    //         {/* Description: {detail.description} */}
+    //         </p>
+
+    //       </div>
+    //     </div>
+    //   )}
+    // </div>
+    <div className="bg-white w-3/4 content-center shadow-lg p-4 lg:ml-60">
       {loading ? (
         "loading"
       ) : (
-        <div className="md:flex">
-          <div className="md:flex-shrink-0">
+        <div className="grid grid-flow-col p-4">
+          <div className="bg-white -mr-60 lg:w-5/6 rounded-lg">
             <img
-              className="h-60 w-full object-cover md:h-full md:w-48"
+              className="h-60 lg:h-72 inline-block align-middle object-cover md:h-full md:w-48 lg:w-60 lg:mb-50"
               src={detail.image}
               alt={detail.title}
             />
           </div>
-          <div className="p-8">
-            <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-              <h1> {detail.title}</h1>
-            </div>
+          <div className="bg-white w-96 rounded-lg text-left text-lg font-bold">
+            <h1 className="text-2xl"> {detail.title}</h1>
             <p> price: K {detail.price} </p>
-            <p> Category: {detail.category} </p>
-            <p className="mt-2 text-gray-500">
-            Description: {detail.description}
+            <p>
+              Category: <pan className="text-gray-400">{detail.category}</pan>{" "}
             </p>
-          
+            <p>
+              {" "}
+              Description:{" "}
+              <span className="text-gray-400 font-bold">
+                {detail.description}{" "}
+              </span>{" "}
+            </p>
           </div>
         </div>
       )}
